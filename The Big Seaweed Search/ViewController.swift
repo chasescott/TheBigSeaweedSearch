1e2dba7a559f88cb494fb68f0c38d5a4d6c154af
chasescott <chase.scott.pearson@gmail.com>
2017-07-05 17:33:15 +0100

    Initial Commit

---
+++ The Big Seaweed Search/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  The Big Seaweed Search
+//
+//  Created by Chase Scott-Pearson on 05/07/2017.
+//  Copyright © 2017 Chase Scott-Pearson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
