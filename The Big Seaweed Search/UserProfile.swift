7d951e57c248825097a1800da2ea3797743c02a0
chasescott <chase.scott.pearson@gmail.com>
2017-07-08 17:21:44 +0100

    Profile view completed, profile image loaded from firebase storage with user name & location

---
+++ The Big Seaweed Search/UserProfile.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  The Big Seaweed Search
+//
+//  Created by Chase Scott-Pearson on 08/07/2017.
+//  Copyright © 2017 Chase Scott-Pearson. All rights reserved.
+//
+
+import Foundation
